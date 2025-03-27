93905d035910ac7f164f13f168c1c6640e65347b
Namdz211 <lqnam0211@gmail.com>
2025-03-27 11:10:04 +0700

    nam

---
+++ Phần Mềm Dinh Dưỡng/Dashboard.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Phần_Mềm_Dinh_Dưỡng
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.btnExit = new Guna.UI2.WinForms.Guna2Button();
+            this.PanelMinisize = new Guna.UI2.WinForms.Guna2Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnAddFromMonAn = new Guna.UI2.WinForms.Guna2Button();
+            this.btnHDSD = new Guna.UI2.WinForms.Guna2Button();
+            this.btnAddFromNganHangThucDon = new Guna.UI2.WinForms.Guna2Button();
+            this.AddFromNguyenLieu = new Guna.UI2.WinForms.Guna2Button();
+            this.btnKtraDD = new Guna.UI2.WinForms.Guna2Button();
+            this.btnNguyenLieu = new Guna.UI2.WinForms.Guna2Button();
+            this.PanelMoving = new Guna.UI2.WinForms.Guna2Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnExit.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnExit.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnExit.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnExit.FillColor = System.Drawing.Color.Red;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageSize = new System.Drawing.Size(25, 25);
+            this.btnExit.Location = new System.Drawing.Point(0, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(37, 36);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // PanelMinisize
+            // 
+            this.PanelMinisize.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.PanelMinisize.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.PanelMinisize.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.PanelMinisize.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.PanelMinisize.FillColor = System.Drawing.Color.Red;
+            this.PanelMinisize.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.PanelMinisize.ForeColor = System.Drawing.Color.White;
+            this.PanelMinisize.Image = ((System.Drawing.Image)(resources.GetObject("PanelMinisize.Image")));
+            this.PanelMinisize.ImageSize = new System.Drawing.Size(25, 25);
+            this.PanelMinisize.Location = new System.Drawing.Point(0, 54);
+            this.PanelMinisize.Name = "PanelMinisize";
+            this.PanelMinisize.Size = new System.Drawing.Size(42, 38);
+            this.PanelMinisize.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.ForeColor = System.Drawing.Color.Red;
+            this.panel2.Location = new System.Drawing.Point(28, 138);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1882, 852);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnAddFromMonAn
+            // 
+            this.btnAddFromMonAn.BackColor = System.Drawing.SystemColors.Window;
+            this.btnAddFromMonAn.BorderRadius = 18;
+            this.btnAddFromMonAn.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnAddFromMonAn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddFromMonAn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddFromMonAn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnAddFromMonAn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnAddFromMonAn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnAddFromMonAn.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddFromMonAn.ForeColor = System.Drawing.Color.Red;
+            this.btnAddFromMonAn.Location = new System.Drawing.Point(648, 17);
+            this.btnAddFromMonAn.Name = "btnAddFromMonAn";
+            this.btnAddFromMonAn.PressedColor = System.Drawing.Color.AntiqueWhite;
+            this.btnAddFromMonAn.Size = new System.Drawing.Size(299, 50);
+            this.btnAddFromMonAn.TabIndex = 2;
+            this.btnAddFromMonAn.Text = "TẠO THỰC ĐƠN TỪ MÓN ĂN";
+            // 
+            // btnHDSD
+            // 
+            this.btnHDSD.BackColor = System.Drawing.Color.White;
+            this.btnHDSD.BorderRadius = 18;
+            this.btnHDSD.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnHDSD.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnHDSD.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnHDSD.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnHDSD.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnHDSD.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnHDSD.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHDSD.ForeColor = System.Drawing.Color.Red;
+            this.btnHDSD.Location = new System.Drawing.Point(19, 17);
+            this.btnHDSD.Name = "btnHDSD";
+            this.btnHDSD.Size = new System.Drawing.Size(250, 50);
+            this.btnHDSD.TabIndex = 0;
+            this.btnHDSD.Text = "HƯỚNG DẪN SỬ DỤNG";
+            this.btnHDSD.Click += new System.EventHandler(this.btnHDSD_Click);
+            // 
+            // btnAddFromNganHangThucDon
+            // 
+            this.btnAddFromNganHangThucDon.BorderRadius = 18;
+            this.btnAddFromNganHangThucDon.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnAddFromNganHangThucDon.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddFromNganHangThucDon.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddFromNganHangThucDon.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnAddFromNganHangThucDon.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnAddFromNganHangThucDon.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnAddFromNganHangThucDon.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddFromNganHangThucDon.ForeColor = System.Drawing.Color.Red;
+            this.btnAddFromNganHangThucDon.Location = new System.Drawing.Point(303, 17);
+            this.btnAddFromNganHangThucDon.Name = "btnAddFromNganHangThucDon";
+            this.btnAddFromNganHangThucDon.Size = new System.Drawing.Size(315, 50);
+            this.btnAddFromNganHangThucDon.TabIndex = 1;
+            this.btnAddFromNganHangThucDon.Text = "TẠO THỰC ĐƠN TỪ NGÂN HÀNG THỰC ĐƠN";
+            // 
+            // AddFromNguyenLieu
+            // 
+            this.AddFromNguyenLieu.BorderRadius = 18;
+            this.AddFromNguyenLieu.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.AddFromNguyenLieu.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.AddFromNguyenLieu.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.AddFromNguyenLieu.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.AddFromNguyenLieu.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.AddFromNguyenLieu.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.AddFromNguyenLieu.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddFromNguyenLieu.ForeColor = System.Drawing.Color.Red;
+            this.AddFromNguyenLieu.Location = new System.Drawing.Point(973, 17);
+            this.AddFromNguyenLieu.Name = "AddFromNguyenLieu";
+            this.AddFromNguyenLieu.Size = new System.Drawing.Size(250, 50);
+            this.AddFromNguyenLieu.TabIndex = 3;
+            this.AddFromNguyenLieu.Text = "TẠO THỰC ĐƠN TỪ NGUYÊN LIỆU";
+            // 
+            // btnKtraDD
+            // 
+            this.btnKtraDD.BorderRadius = 18;
+            this.btnKtraDD.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnKtraDD.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnKtraDD.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnKtraDD.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnKtraDD.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnKtraDD.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnKtraDD.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKtraDD.ForeColor = System.Drawing.Color.Red;
+            this.btnKtraDD.Location = new System.Drawing.Point(1247, 17);
+            this.btnKtraDD.Name = "btnKtraDD";
+            this.btnKtraDD.Size = new System.Drawing.Size(295, 50);
+            this.btnKtraDD.TabIndex = 4;
+            this.btnKtraDD.Text = "KIỂM TRA DINH DƯỠNG CỦA THỰC ĐƠN";
+            // 
+            // btnNguyenLieu
+            // 
+            this.btnNguyenLieu.BorderRadius = 18;
+            this.btnNguyenLieu.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnNguyenLieu.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnNguyenLieu.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnNguyenLieu.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnNguyenLieu.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnNguyenLieu.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnNguyenLieu.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNguyenLieu.ForeColor = System.Drawing.Color.Red;
+            this.btnNguyenLieu.Location = new System.Drawing.Point(1569, 17);
+            this.btnNguyenLieu.Name = "btnNguyenLieu";
+            this.btnNguyenLieu.Size = new System.Drawing.Size(250, 50);
+            this.btnNguyenLieu.TabIndex = 5;
+            this.btnNguyenLieu.Text = "DANH MỤC NGUYÊN LIỆU";
+            // 
+            // PanelMoving
+            // 
+            this.PanelMoving.BackColor = System.Drawing.Color.Red;
+            this.PanelMoving.Location = new System.Drawing.Point(19, 73);
+            this.PanelMoving.Name = "PanelMoving";
+            this.PanelMoving.Size = new System.Drawing.Size(250, 7);
+            this.PanelMoving.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.PanelMoving);
+            this.panel1.Controls.Add(this.btnNguyenLieu);
+            this.panel1.Controls.Add(this.btnKtraDD);
+            this.panel1.Controls.Add(this.AddFromNguyenLieu);
+            this.panel1.Controls.Add(this.btnAddFromMonAn);
+            this.panel1.Controls.Add(this.btnAddFromNganHangThucDon);
+            this.panel1.Controls.Add(this.btnHDSD);
+            this.panel1.Location = new System.Drawing.Point(74, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1836, 95);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(26, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1390, 651);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(1942, 1102);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PanelMinisize);
+            this.Controls.Add(this.btnExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button btnExit;
+        private Guna.UI2.WinForms.Guna2Button PanelMinisize;
+        private System.Windows.Forms.Panel panel2;
+        private Guna.UI2.WinForms.Guna2Button btnAddFromMonAn;
+        private Guna.UI2.WinForms.Guna2Panel PanelMoving;
+        private Guna.UI2.WinForms.Guna2Button btnKtraDD;
+        private Guna.UI2.WinForms.Guna2Button AddFromNguyenLieu;
+        private Guna.UI2.WinForms.Guna2Button btnAddFromNganHangThucDon;
+        private Guna.UI2.WinForms.Guna2Button btnHDSD;
+        private Guna.UI2.WinForms.Guna2Button btnNguyenLieu;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
